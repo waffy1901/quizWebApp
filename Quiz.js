b704fe925bedf6a3241b0b18587164b8dafd1af5
waffy1901 <waffyahmed@gmail.com>
2023-07-01 00:09:14 -0400

    Added a JS file and refactored CSS

---
+++ Quiz.js
@@ -0,0 +1,48 @@
+const questions = [{
+        question: "What percentage of a human brain consists of fat?",
+        A: "60%",
+        B: "20%",
+        C: "50%",
+        D: "93%",
+        correctAnswer: "A"
+    }, {
+        question: "What was the hottest temperature ever recorded on Earth?",
+        A: "2,000,000,000&deg; F",
+        B: "2,000,000,000&deg; K",
+        C: "76,983,456&deg; C",
+        D: "20,456,763&deg; F",
+        correctAnswer: "B"
+    }, {
+        question: "Where is the hottest inhabitable place on Earth?",
+        A: "Sahara Desert",
+        B: "Death Valley",
+        C: "Dallol, Ethiopia",
+        D: "Dasht-e Lut",
+        correctAnswer: "C"
+    }, {
+        question: "Which animal is known for having the largest eyes among all animals?",
+        A: "Giant Squids",
+        B: "Lemurs",
+        C: "Sphynx Cats",
+        D: "Ostriches",
+        correctAnswer: "A"
+    }, {
+        question: "What is the only planet to spin clockwise?",
+        A: "Mars",
+        B: "Neptune",
+        C: "Earth",
+        D: "Venus",
+        correctAnswer: "D"
+    }, {
+        question: "What is the hashtag formally known as?",
+        A: "Pound",
+        B: "Octothorpe",
+        C: "Hash",
+        D: "Tag",
+        correctAnswer: "B"
+    }, {
+        question: "An ultracrepidarian is defined as someone who voices thoughts beyond their expertise.",
+        A: "True",
+        B: "False",
+        correctAnswer: "A"
+    },]
